ebdff694116dba48861f8806dfefb64e92a4db9d
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2021-04-28 23:18:47 -0300

    Add ex_docs and lib info for publishing it to HEX

--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule OpenTelemetry.MixProject do
   use Mix.Project
 
+  @repo_url "https://github.com/jeffsta/open_telemetry_elixir"
+
   def project do
     [
       app: :open_telemetry,
@@ -8,7 +10,11 @@ defmodule OpenTelemetry.MixProject do
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      source_url: "https://github.com/jeffsta/open_telemetry_elixir"
+      source_url: @repo_url,
+      description: description(),
+      license: "MIT",
+      links: "",
+      package: package()
     ]
   end
 
@@ -20,6 +26,24 @@ defmodule OpenTelemetry.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Open Telemetry SDK for Elixir.
+    OTEL project: https://opentelemetry.io/
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["Jefferson Stachelski"],
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url}
+    ]
   end
 end
